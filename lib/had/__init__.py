0377075a01e10699119c0ae5fbe0c19daa887698
h-akira <79441679+h-akira@users.noreply.github.com>
2024-08-27 22:37:07 +0900

    update init

--- lib/had/__init__.py
+++ lib/had/__init__.py
@@ -1,5 +1,5 @@
-from . import conf, shourtcuts, authenticate, urls, handler
+from . import conf, shourtcuts, authenticate, urls, handler, cfn, scripts
 # 準備中
-# from . import db, forms
+from . import db, forms
 
-__all__ = ['conf', 'shourtcuts', 'authenticate', 'urls', 'handler', 'db', 'forms']
+__all__ = ['conf', 'shourtcuts', 'authenticate', 'urls', 'handler', 'cfn', 'scripts', 'db', 'forms']
