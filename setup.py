07a5187d47692aa6eff6f6ca09b76da071c9b54e
h-akira <79441679+h-akira@users.noreply.github.com>
2024-11-17 21:34:39 +0900

    upgrade version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements_from_file(file_name):
 
 setup(
   name='had',
-  version='1.0.2',
+  version='1.0.3',
   package_dir={"":"lib"},
   packages=find_packages(where="lib"),
   scripts=glob.glob("bin/*.py"),
